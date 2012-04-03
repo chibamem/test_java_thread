6f4e456d6df42eab0b4fb5f0532a3627cecabf40
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-04-03 21:28:59 +0900

    p256のObservableSet256に追記

--- ObservableSet256.java
+++ ObservableSet256.java
@@ -1,8 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import javax.print.attribute.SetOfIntegerSyntax;
 
 
 
 public class ObservableSet256<E> extends ForwardingSet<E> {
+    public ObservableSet256(Set<E> set) {
+        super(set);
+    }
+
+    private final List<SetObserver<E> observers = new ArrayList<SetObserver<E>>();
+
+    public void addObserver(SetObserver<E> observer) {
+        synchronized (observers) {
+            observers.add(observer);
+        }
+    }
+
+    public boolean removeObserver(SetObserver<E> observer) {
+        synchronized (observers) {
+            return observer.remove(observer);
+        }
+    }
+
+    private void notifyElementAdded(E element) {
+        synchronized (observers) {
+            for (SetObserver<E> observer : observers)
+                observer.added(this, element);
+        }
+    }
+
+    @Override
+    public boolean add(E element) {
+        boolean added = super.add(element);
+        if (added)
+            notifyElementAdded(element);
+        return added;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        boolean result = false;
+        for (E element : c)
+            result |= add(element);// notifyElementAdded呼び出し
+        return result;
+    }
 
 	private static volatile boolean stopRequested;
 	
